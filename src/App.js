6b7fdaf6f6315025e38af3cf806f7bd160d81401
olcaneristi <ogulcaneristi123@gmail.com>
2020-09-29 02:44:06 +0300

    navbar icin route olusturuldu

--- src/App.js
+++ src/App.js
@@ -1,58 +1,23 @@
 import React, { useState } from "react"
 import "./styles/index.scss"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 import Header from "./components/Header"
-import Story from "./components/Story"
-import Posts from "./components/Posts"
+import { Home, Explore, Inbox, Profile, Like } from "./pages"
 
 export default function App() {
-  const [posts, setPosts] = useState([
-    {
-      Avatar: "https://avatarfiles.alphacoders.com/145/145932.jpg",
-      username: "olcaneristi",
-      postLocation: "Brooklyn City",
-      postImage:
-        "https://papers.co/wallpaper/papers.co-mn29-bridge-city-river-night-view-nature-32-wallpaper.jpg",
-      caption: "it works!",
-      likeCount: "1.345"
-    },
-    {
-      Avatar: "https://avatarfiles.alphacoders.com/145/145932.jpg",
-      username: "olcaneristi",
-      postLocation: "Mount Everest",
-      postImage:
-        "https://papers.co/wallpaper/papers.co-nw48-luca-bravo-mountain-sunset-nature-32-wallpaper.jpg",
-      caption: "beauty.",
-      likeCount: "3.232"
-    },
-    {
-      Avatar: "https://avatarfiles.alphacoders.com/145/145932.jpg",
-      username: "olcaneristi",
-      postLocation: "Dubai",
-      postImage:
-        "https://papers.co/wallpaper/papers.co-nv99-city-night-light-car-dark-nature-32-wallpaper.jpg",
-      caption: "wow!",
-      likeCount: "4.412"
-    }
-  ])
-
   return (
     <div className="App">
-      <Header />
-      <Story
-        name="olcaneristi"
-        avatar="https://avatarfiles.alphacoders.com/145/145932.jpg"
-      />
-      {posts.map((posts) => (
-        <Posts
-          Avatar={posts.Avatar}
-          username={posts.username}
-          postLocation={posts.postLocation}
-          postImage={posts.postImage}
-          caption={posts.caption}
-          likeCount={posts.likeCount}
-        />
-      ))}
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/inbox" component={Inbox} />
+          <Route path="/explore" component={Explore} />
+          <Route path="/like" component={Like} />
+          <Route path="/profile" component={Profile} />
+        </Switch>
+      </Router>
     </div>
   )
 }
