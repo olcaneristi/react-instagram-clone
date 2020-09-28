c7b76fe4a12149cc8c1f7c67f65673731160a71a
olcaneristi <ogulcaneristi123@gmail.com>
2020-09-29 02:45:30 +0300

    sayfalari tek yerden export etmek icin index eklendi

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from "./home"
+import Inbox from "./inbox"
+import Explore from "./explore"
+import Profile from "./profile"
+import Like from "./like"
+
+export { Home, Inbox, Explore, Profile, Like }
