c19ec90cb6cd1682a7a94d9a05bd0ecf0fc1d806
olcaneristi <ogulcaneristi123@gmail.com>
2020-11-03 20:11:28 +0300

    add loading and layout animation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import "./styles/index.scss"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-
+import Loading from "./components/icons/Loading"
 import Header from "./components/Header"
 import { Home, Explore, Inbox, Profile, Like } from "./pages"
 
 export default function App() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1200)
+  }, [])
   return (
-    <div className="App">
-      <Router>
-        <Header />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/inbox" component={Inbox} />
-          <Route path="/explore" component={Explore} />
-          <Route path="/like" component={Like} />
-          <Route path="/profile" component={Profile} />
-        </Switch>
-      </Router>
-    </div>
+    <>
+      {loading === false ? (
+        <div className="App">
+          <Router>
+            <Header />
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/inbox" component={Inbox} />
+              <Route path="/explore" component={Explore} />
+              <Route path="/like" component={Like} />
+              <Route path="/profile" component={Profile} />
+            </Switch>
+          </Router>
+        </div>
+      ) : (
+        <Loading />
+      )}
+    </>
   )
 }
